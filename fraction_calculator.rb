8336230f1f2fd93b7ada64d402a54868635a6a72
moof mayeda <moofmayeda@yahoo.com>
2022-02-09 14:07:52 -0800

    Get input from user

--- fraction_calculator.rb
+++ fraction_calculator.rb
@@ -1,1 +1,9 @@
-puts "Welcome!"
+input = nil
+while input != "quit"
+  if input
+    puts "Your result is: #{input}"
+  end
+  puts "Enter a calculation or type 'quit' to exit. What would you like to calculate?"
+  input = gets.chomp
+end
+puts "Goodbye!"
